7bd240d80b4b5d40f5cd7bdff462f1ba08ffd5a6
Ph3bian <kudi@kudi-inc.local>
2019-12-14 02:54:33 +0100

    Using props

--- App.js
+++ App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
+const DisplayName = ({ name }) => {
+    return (
+        <View style={styles.container}>
+            <Text>Hello {name}</Text>
+        </View>
+    )
+}
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello World From Ph3bian</Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <DisplayName name='Ph3bian' />
+            <DisplayName name='Jesus' />
+            <DisplayName name='Samantha' />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
